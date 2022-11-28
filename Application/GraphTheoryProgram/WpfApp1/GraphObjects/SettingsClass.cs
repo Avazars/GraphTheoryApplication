f8f923e64001a63b89a9e7a558a15a4e25056744
JSadler <jadin.sadler@wsu.edu>
2022-11-28 15:18:50 -0800

    added basic start for the application

---
+++ Application/GraphTheoryProgram/WpfApp1/GraphObjects/SettingsClass.cs
@@ -0,0 +1,6 @@
+﻿namespace WpfApp1.GraphObjects;
+
+public class SettingsClass
+{
+    
+}
---
+++ Application/GraphTheoryProgram/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<Ellipse> _ellipses = new List<Ellipse>();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawEllipses()
+        {
+            foreach (var node in _ellipses)
+            {
+                if (!DrawingGrid.Children.Contains(node))
+                    DrawingGrid.Children.Add(node);
+            }
+        }
+        
+        private Ellipse MakeEllipse()
+        {
+            int size = 100;
+            
+            Ellipse ellip = new Ellipse
+            {
+                Stroke = Brushes.White,
+                Fill = Brushes.White,
+                StrokeThickness = 2,
+                Width = size,
+                Height =size,
+                RenderTransform = new TranslateTransform(-(size/2),-(size/2))
+            };
+            ellip.MouseRightButtonDown += DeleteEllipse;
+            return ellip;
+        }
+
+        private void AddEllipse(double x, double y)
+        {
+            Ellipse temp = MakeEllipse();
+            Canvas.SetLeft(temp, x);
+            Canvas.SetTop(temp, y);
+            _ellipses.Add(temp);
+            DrawEllipses();
+        }
+
+        
+        private void DeleteEllipse(object sender, MouseEventArgs e)
+        {
+            var oop = sender as Ellipse;
+
+            if (DrawingGrid.Children.Contains(oop) && oop != null)
+            {
+                _ellipses.Remove(oop);
+                DrawingGrid.Children.Remove(oop);
+            }
+        }
+        private void DrawingGrid_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            AddEllipse(e.GetPosition(DrawingGrid).X, e.GetPosition(DrawingGrid).Y);
+        }
+    }
+}
