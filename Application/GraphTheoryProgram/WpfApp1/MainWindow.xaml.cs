38cb177faf209e0ed6d6bab3b69378ab728932ee
JSadler <jadin.sadler@wsu.edu>
2022-11-29 14:45:20 -0800

    transfering to other computer

--- Application/GraphTheoryProgram/WpfApp1/MainWindow.xaml.cs
+++ Application/GraphTheoryProgram/WpfApp1/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1
@@ -39,7 +31,7 @@ namespace WpfApp1
         
         private Ellipse MakeEllipse()
         {
-            int size = 100;
+            int size = 50;
             
             Ellipse ellip = new Ellipse
             {
@@ -47,7 +39,7 @@ namespace WpfApp1
                 Fill = Brushes.White,
                 StrokeThickness = 2,
                 Width = size,
-                Height =size,
+                Height = size,
                 RenderTransform = new TranslateTransform(-(size/2),-(size/2))
             };
             ellip.MouseRightButtonDown += DeleteEllipse;
