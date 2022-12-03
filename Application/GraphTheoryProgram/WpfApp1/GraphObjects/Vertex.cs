ddb11ed4194355a15e60ef9720cebcbe12d36d24
JSadler <jadin.sadler@wsu.edu>
2022-12-02 18:06:16 -0800

    pushing to other computer

--- Application/GraphTheoryProgram/WpfApp1/GraphObjects/Vertex.cs
+++ Application/GraphTheoryProgram/WpfApp1/GraphObjects/Vertex.cs
@@ -5,55 +5,7 @@ using System.Windows.Shapes;
 
 namespace WpfApp1.GraphObjects;
 
-public class Vertex
-{
-    private const int vertexSize = 50;
-    private Canvas displayCanvas { get; }
-
-    public Vertex()
-    {
-        displayCanvas = MakeNewDisplay("");
-    }
-    
-    public Vertex(string label)
-    {
-        displayCanvas = MakeNewDisplay(label);
-    }
-
-    private Canvas MakeNewDisplay(string label)
-    {
-
-        Canvas returnCanvas = new Canvas()
-        {
-            
-        };
-        
-        Ellipse ellipse = new Ellipse()
-        {
-            Stroke = Brushes.White,
-            Fill = Brushes.White,
-            StrokeThickness = 2,
-            Width = vertexSize,
-            Height = vertexSize,
-            RenderTransform = new TranslateTransform(-(vertexSize/2),-(vertexSize/2))
-        };
-        
-        TextBlock textBlock = new TextBlock()
-        {
-                TextAlignment = TextAlignment.Center,
-                Text = label
-        };
-
-        returnCanvas.Children.Add(ellipse);
-        returnCanvas.Children.Add(textBlock);
-        
-        return returnCanvas;
-    }
-    
-    public void UpdateLabel(string newLabel)
-    {
-        
-    }
-    
-    
+public class Vertex {
+    public double posX { get; private set; }
+    public double posY { get; private set; }
 }
--- Application/GraphTheoryProgram/WpfApp1/MainWindow.xaml.cs
+++ Application/GraphTheoryProgram/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
@@ -27,15 +28,26 @@ namespace WpfApp1
             Color
         }
 
+
+        private Dictionary<Ellipse, Vertex> _verticesDictionary = new Dictionary<Ellipse, Vertex>();
+        
         private List<Vertex> vertices = new List<Vertex>();
         private List<Ellipse> _ellipses = new List<Ellipse>();
+        private List<Path> _paths = new List<Path>();
+        
         private ToolSet currentToolSet;
         private Point? DragStart = null;
+        private Color selectionColor = Colors.DodgerBlue;
+        private List<Tuple<Ellipse, Ellipse>> listOfEdges;
+        private Ellipse SelectedVertex = null;
         
-        
+        private const int ellipseSize = 50;
+
         public MainWindow()
         {
             InitializeComponent();
+            colPic.SelectedColor = Colors.Azure;
+            colPic.SecondaryColor = Colors.Black;
         }
         private void topPanelMouseDown(object sender, MouseEventArgs e)
         {
@@ -44,25 +56,7 @@ namespace WpfApp1
         
         
         
-        private Ellipse MakeEllipse()
-        {
-            const int size = 50;
-            
-            Ellipse ellipse = new Ellipse
-            {
-                Stroke = Brushes.White,
-                Fill = Brushes.White,
-                StrokeThickness = 2,
-                Width = size,
-                Height = size,
-                RenderTransform = new TranslateTransform(-(size/2),-(size/2))
-            };
-            ellipse.MouseLeftButtonDown += VertexClicked;
-            ellipse.MouseMove += VertexDragged;
-            ellipse.MouseLeftButtonUp += VertexReleased;
-            
-            return ellipse;
-        }
+        
 
         private void VertexDragged(object sender, MouseEventArgs e)
         {
@@ -84,25 +78,122 @@ namespace WpfApp1
 
         private void VertexClicked(object sender, MouseButtonEventArgs e)
         {
-            var selectedVertex = sender as Ellipse;
+            
+            var ClickedVertex = sender as Ellipse;
             switch (currentToolSet)
             {
                 case ToolSet.Remove:
-                    DeleteEllipse(selectedVertex);
+                    DeleteEllipse(ClickedVertex);
                     break;
                 case ToolSet.Move:
-                    DragStart = e.GetPosition(selectedVertex);
-                    selectedVertex.CaptureMouse();
+                    DragStart = e.GetPosition(ClickedVertex);
+                    ClickedVertex.CaptureMouse();
                     break;
                 case ToolSet.Edge:
-                    
+                    if (SelectedVertex == null)
+                    {
+                        SelectedVertex = ClickedVertex;
+                        ClickedVertex.Stroke = new SolidColorBrush(selectionColor);
+                    }
+                    else
+                    {
+                        double posX1 =  Canvas.GetLeft(SelectedVertex);
+                        double posY1 = Canvas.GetTop(SelectedVertex);
+                        
+                        double posX2 = Canvas.GetLeft(ClickedVertex);
+                        double posY2 = Canvas.GetTop(ClickedVertex);
+
+                        Path temp = new Path() { Stroke = new SolidColorBrush(colPic.SelectedColor), StrokeThickness = 6};
+                        
+                        if (SelectedVertex == ClickedVertex)
+                        {
+                            temp.Data = new EllipseGeometry(new Point(ellipseSize/1.5+posX1,ellipseSize/1.5+posY1),ellipseSize,ellipseSize);
+                            _paths.Add(temp);
+                        }
+                        else
+                        {
+                            PathFigure pf = new PathFigure()
+                            {
+                                StartPoint = new Point(posX1, posY1),
+                                Segments = new PathSegmentCollection(){new ArcSegment()
+                                {Size = new Size(100,100), Point = new Point(posX2,posY2)}}
+                            };
+                            PathGeometry pg = new PathGeometry
+                            {
+                                Transform = null,
+                                Figures = new PathFigureCollection() {pf},
+                                FillRule = FillRule.EvenOdd
+                            };
+                            temp.Data = pg;
+                            //temp.Data = new LineGeometry(new Point(posX1,posY1),new Point(posX2,posY2));
+                            _paths.Add(temp);
+                        }
+                        temp.MouseLeftButtonDown += EdgeClicked;
+                        temp.MouseMove += EdgeDragged;
+                        temp.MouseLeftButtonUp += EdgeReleased;
+                        DrawingGrid.Children.Add(temp);
+                        Canvas.SetZIndex(temp, 0);
+                        RecalculateInfoPanel();
+                        
+                        SelectedVertex.Stroke = new SolidColorBrush(Colors.Black);
+                        SelectedVertex = null;
+                    }
                     break;
                 case ToolSet.Color:
-                    
+                    ClickedVertex.Fill = new SolidColorBrush(colPic.SelectedColor);
                     break;
             }
         }
         
+        private void RemoveLine(Path pt)
+        {
+            if (DrawingGrid.Children.Contains(pt))
+            {
+                _paths.Remove(pt);
+                DrawingGrid.Children.Remove(pt);
+            }
+            RecalculateInfoPanel();
+        }
+        
+        private void EdgeClicked(object sender, MouseButtonEventArgs e)
+        {
+            var ClickedLine = sender as Path;
+            switch (currentToolSet)
+            {
+                case ToolSet.Move:
+                    DragStart = e.GetPosition(ClickedLine);
+                    ClickedLine.CaptureMouse();
+                    break;
+                case ToolSet.Vertex:
+                    break;
+                case ToolSet.Edge:
+                    break;
+                case ToolSet.Remove:
+                    RemoveLine(ClickedLine);
+                    break;
+                case ToolSet.Color:
+                    ClickedLine.Stroke = new SolidColorBrush(colPic.SelectedColor);
+                    break;
+            }
+        }
+        
+        private void EdgeDragged(object sender, MouseEventArgs e)
+        {
+            if (DragStart != null && e.LeftButton == MouseButtonState.Pressed)
+            {
+                var element = (UIElement) sender;
+                var p2 = e.GetPosition(DrawingGrid);
+                Canvas.SetLeft(element, p2.X - DragStart.Value.X);
+                Canvas.SetTop(element, p2.Y - DragStart.Value.Y);
+            }
+        }
+        
+        private void EdgeReleased(object sender, MouseButtonEventArgs e)
+        {
+            var element = sender as Path;
+            DragStart = null;
+            element.ReleaseMouseCapture();
+        }
         
         private void AddVertex(double x, double y)
         {
@@ -111,18 +202,29 @@ namespace WpfApp1
             Canvas.SetLeft(temp, x);
             Canvas.SetTop(temp, y);
             _ellipses.Add(temp);
-            DrawEllipses();
-        }
-
-        private void DrawEllipses()
-        {
-            foreach (var node in _ellipses.Where(node => !DrawingGrid.Children.Contains(node)))
-            {
-                DrawingGrid.Children.Add(node);
-            }
+            DrawingGrid.Children.Add(temp);
+            Canvas.SetZIndex(temp, 10);
             RecalculateInfoPanel();
         }
         
+        private Ellipse MakeEllipse()
+        {
+            Ellipse ellipse = new Ellipse
+            {
+                Stroke = Brushes.White,
+                Fill = Brushes.White,
+                StrokeThickness = 2,
+                Width = ellipseSize,
+                Height = ellipseSize,
+                RenderTransform = new TranslateTransform(-(ellipseSize/2),-(ellipseSize/2))
+            };
+            ellipse.MouseLeftButtonDown += VertexClicked;
+            ellipse.MouseMove += VertexDragged;
+            ellipse.MouseLeftButtonUp += VertexReleased;
+            
+            return ellipse;
+        }
+
         private void DeleteEllipse(Ellipse oop)
         {
             if (DrawingGrid.Children.Contains(oop))
@@ -136,7 +238,7 @@ namespace WpfApp1
         private void RecalculateInfoPanel()
         {
             int numVertices = _ellipses.Count;
-            int numEdges = 0;
+            int numEdges = _paths.Count;
             int connectivity = 0;
             NumVerticesTextBlock.Text = "n = " + numVertices;
             NumEdgesTextBlock.Text = "m = " + numEdges;
